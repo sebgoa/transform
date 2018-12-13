5e61be324c0ec575d6605318ba6a8d5df5e8b253
sebgoa <runseb@gmail.com>
2018-12-13 22:45:13 +0100

    dfhf

--- hello/hello.js
+++ hello/hello.js
@@ -23,7 +23,7 @@ var exports = module.exports = function(name) {
     if (typeof name == 'object') {
         // Riff's runtime gives you an object even if data is POSTed without Content-Type, but it's an odd object
         if (Object.keys(name).length === 1 && name[Object.keys(name)[0]] === "") {
-            name = JSON.stringify(name) + ' (you might want to try POSTing with a Content-Type header)';
+            name = name;
         } else {
             name = JSON.stringify(name);
         }
