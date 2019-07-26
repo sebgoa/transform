1d591ba79229c8aa982c412ca2f136c58dbec9d6
sebgoa <runseb@gmail.com>
2019-07-26 15:04:11 +0200

    cleanup for docs

--- python/handler.py
+++ python/handler.py
@@ -6,11 +6,9 @@ import datetime
 def endpoint(event, context):
     current_time = datetime.datetime.now().time()
     body = {
-        "message": "Hello Ronny, time is " + str(current_time)
+        "message": "Hello, time is " + str(current_time)
     }
 
-    response = {
-        "body": json.dumps(body)
-    }
+    response = json.dumps(body)
 
     return response
--- simple/README.md
+++
@@ -1,3 +0,0 @@
-# Simple knative example
-
-From Mark Chmarny [repo](https://github.com/mchmarny/simple-app)
