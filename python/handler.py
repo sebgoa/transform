e2dd84725e014a369cbeeb188bf69344be87b8e4
sebgoa <runseb@gmail.com>
2020-04-09 14:14:57 +0200

    Update handler.py

--- python/handler.py
+++ python/handler.py
@@ -6,7 +6,7 @@ import datetime
 def endpoint(event, context):
     current_time = datetime.datetime.now().time()
     body = {
-        "message": "Hello, disco " + str(current_time)
+        "message": "Hello, demo " + str(current_time)
     }
 
     response = json.dumps(body)
