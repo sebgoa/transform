b14b8ba2d02b7cb2a32d2552f508e895847ad2fc
sebgoa <runseb@gmail.com>
2020-05-15 15:03:12 +0200

    Update handler.py

--- python/handler.py
+++ python/handler.py
@@ -1,12 +1,21 @@
 
 import json
-import datetime
-
+import requests
 
 def endpoint(event, context):
     #current_time = datetime.datetime.now().time()
     print(event.keys())
-    body = {
+    
+    # respond with a cloudevent
+    body = {}
+    body['specversion']='1.0'
+    body['time']='2018-04-05T03:56:24Z'
+    body['id']='XXX-YYY-ZZZ-WWW'
+    body['datacontenttype']='Content-type: application/json'
+
+    #headers = {'Content-Type': 'application/cloudevents+json'}
+    
+    body['data'] = {
         "message": "A commit was made at " + event['repository']['full_name']
     }
 
