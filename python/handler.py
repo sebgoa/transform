3e17a375e986d5f14a4aa1084b9ab1492865dd7c
sebgoa <runseb@gmail.com>
2020-05-15 14:03:33 +0200

    Update handler.py

--- python/handler.py
+++ python/handler.py
@@ -5,7 +5,7 @@ import datetime
 
 def endpoint(event, context):
     #current_time = datetime.datetime.now().time()
-    print(json.loads(event))
+    print(event['body'])
     body = {
         "message": "Hello from repo " + event['body']['repository']['full_name']
     }
